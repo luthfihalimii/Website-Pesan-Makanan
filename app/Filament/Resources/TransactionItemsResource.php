f5d10c2e4053367a3a4d9522d7573b0b085526e8
Luthfi Halimi <qlatifah62@gail.com>
2025-02-19 04:07:53 +0700

    Transaction Items & Resource

--- app/Filament/Resources/TransactionItemsResource.php
+++ app/Filament/Resources/TransactionItemsResource.php
@@ -11,13 +11,37 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Contracts\Support\Htmlable;
 
 class TransactionItemsResource extends Resource
 {
     protected static ?string $model = TransactionItems::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function shouldRegisterNavigation() : bool
+    {
+        return false;
+    }
+
+    public static string $parentResource = TransactionResource::class;
+
+    public static function getRecordTitle(?Model $record): string|null|Htmlable
+    {
+        return $record->title;
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 
     public static function form(Form $form): Form
     {
@@ -46,21 +70,6 @@ class TransactionItemsResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('transaction_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('foods_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('quantity')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('price')
-                    ->money()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('subtotal')
-                    ->numeric()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -69,18 +78,32 @@ class TransactionItemsResource extends Resource
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('transaction_id')
+                    ->numeric()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('food.name')
+                    ->label('Food Name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money('IDR')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('subtotal')
+                    ->numeric()
+                    ->money('IDR')
+                    ->sortable(),
+
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
             ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->bulkActions([]);
     }
 
     public static function getRelations(): array
@@ -94,8 +117,6 @@ class TransactionItemsResource extends Resource
     {
         return [
             'index' => Pages\ListTransactionItems::route('/'),
-            'create' => Pages\CreateTransactionItems::route('/create'),
-            'edit' => Pages\EditTransactionItems::route('/{record}/edit'),
         ];
     }
 }
